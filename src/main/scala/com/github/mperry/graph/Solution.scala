08b4fb3ef839b183e52419095da89ef90fbee081
Mark Perry <maperry78@yahoo.com.au>
2014-03-29 21:42:09 +1000

    Got it working from Gradle

--- src/main/scala/com/github/mperry/graph/Solution.scala
+++ src/main/scala/com/github/mperry/graph/Solution.scala
@@ -2,8 +2,8 @@ package com.github.mperry.graph
 
 object Solution {
 
-  def main(args: Array[String]) {
-    println("Hi from scala")
+  def main(args: Array[String]) = {
+    println("Hi from scala2")
   }
 
 
