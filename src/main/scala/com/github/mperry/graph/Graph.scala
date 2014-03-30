af2efcd69e4d2d43c3ad1601fba98308944faf85
Mark Perry <maperry78@yahoo.com.au>
2014-03-30 15:50:35 +1000

    Tidy up code

--- src/main/scala/com/github/mperry/graph/Graph.scala
+++ src/main/scala/com/github/mperry/graph/Graph.scala
@@ -6,19 +6,15 @@ object Graph {
 		m.get(Node(n)).map(distance(_))
 	}
 
-	def process(m: Map[String, List[(String, Weight)]]): (Set[Node], Set[Edge]) = {
-
-		def nodes = Set.empty[Node]
-		def edges = Set.empty[Edge]
-
-
-		m.foldLeft((nodes, edges))((acc, kv) => {
+	def process(g: SimpleGraph): (Set[Node], Set[Edge]) = {
+		val t = (Set.empty[Node], Set.empty[Edge])
+		g.foldLeft(t)((acc, kv) => {
 			acc match {
-				case (ns, es) => {
+				case (nodes, edges) => {
 					def name1 = kv._1
 					def list = kv._2
 					val n1 = Node(name1)
-					list.foldLeft((ns + n1, es)) {
+					list.foldLeft((nodes + n1, edges)) {
 						case ((nodes2, edges2), (name2, w)) =>
 							val n2 = Node(name2)
 							val e = createEdge(n1, n2, w)
@@ -26,17 +22,12 @@ object Graph {
 					}
 				}
 			}
-
 		})
-		//		(nodes, edges)
 	}
 
 	def toGraph(g: SimpleGraph): Graph = {
-
 		val t = process(g)
-		//		val m = edgeMap(t._2)
 		Graph(t._1, edgeMap(t._2))
-
 	}
 
 	def edgeMap(edges: Set[Edge]): EdgeMap = {
@@ -45,27 +36,22 @@ object Graph {
 		})
 	}
 
-	def edgeMap(m: Map[Node, List[Edge]], n: Node, e: Edge): PathMap = {
-
-		// deal with from first
+	def edgeMap(m: EdgeMap, n: Node, e: Edge): EdgeMap = {
 		val o = m.get(n)
 		val edgeList = o.map(list => {
 			e :: list
 		}).getOrElse(List(e))
-
 		m + ((n, edgeList))
 	}
 
-	def createEdge(x: String, y: String, w: Weight): Edge = {
+	def createEdge(x: NodeId, y: NodeId, w: Weight): Edge = {
 		createEdge(Node(x), Node(y), w)
 	}
-
 
 	def createEdge(x: Node, y: Node, w: Weight): Edge = {
 		val t = if (x.name < y.name) (x, y) else (y, x)
 		Edge(t._1, t._2, w)
 	}
-
 
 	def distance(list: List[Edge]): Weight = {
 		list.foldLeft(0)((d, e) => d + e.distance)
@@ -75,26 +61,18 @@ object Graph {
 		map.get(node).map(distance(_))
 	}
 
+	def nextNode(current: Node, edge: Edge): Node = {
+		if (edge.from == current) edge.to else edge.from
+	}
+
+
+
 }
 
 case class Graph(nodes: Set[Node], edges: EdgeMap) {
 
 	import Graph._
 
-	def nextNode(current: Node, edge: Edge): Node = {
-		if (edge.from == current) edge.to else edge.from
-	}
-
-	def compute(node: Node, oldDist: Option[Weight], newDist: Weight, distances: PathMap, list: List[Edge]): (PathMap, Boolean) = {
-		oldDist.map(x => {
-			if (x <= newDist) {
-				(distances, false)
-			} else {
-				(distances.+((node, list)), true)
-			}
-		}).getOrElse((distances + ((node, list)), true))
-
-	}
 
 	def shortestPath(from: NodeId, to: NodeId): PathMap = {
 		shortestPath(Node(from), Node(to))
@@ -109,34 +87,47 @@ case class Graph(nodes: Set[Node], edges: EdgeMap) {
 	 * overflow for very large graphs
 	 */
 	def shortestPath(from: Node, current: Node, to: Node, distances: PathMap): PathMap = {
-		// lookup paths from this node
 		if (current == to) {
 			distances
 		} else {
 			val nextEdges = edges.get(current)
-			val newMap = nextEdges.map(_.foldLeft(distances)((dm, e) => {
-				val nn = nextNode(current, e)
-				val t = newDistanceMap(dm, current, e, nn)
+			val newMap = nextEdges.map(_.foldLeft(distances)((pathMap, e) => {
+				val next = nextNode(current, e)
+				val t = computePath(pathMap, current, e, next)
 				t match {
-					case (dm2, b) => if (!b) dm2 else shortestPath(from, nn, to, dm2)
+					case (map, b) => if (!b) map else shortestPath(from, next, to, map)
 				}
-
 			}))
 			newMap.getOrElse(distances)
 		}
-
 	}
 
-	def newDistanceMap(dm: PathMap, current: Node, e: Edge, nn: Node): (PathMap, Boolean) = {
-		val o1 = Graph.distance(dm, current).map(_ + e.distance)
-		val oldWay = distance(dm, nn)
-		val o3 = dm.get(current).map(e :: _)
-		val dm2 = for {
-			distanceThisWay <- o1
-			//      oldWay <- o2
-			newList <- o3
-		} yield (compute(nn, oldWay, distanceThisWay, dm, newList))
-		dm2.getOrElse((dm, true))
+	/**
+	 * Returns a tuple of the path map and whether this new route is shorter and
+	 * should be followed further
+	 */
+	def computePath(map: PathMap, current: Node, e: Edge, next: Node): (PathMap, Boolean) = {
+		val previousDistance = distance(map, next) 
+		val tuple = for {
+			distanceThisWay <- distance(map, current).map(_ + e.distance)
+			newList <- map.get(current).map(e :: _)
+		} yield (computePath(next, previousDistance, distanceThisWay, map, newList))
+		tuple.getOrElse((map, true))
+	}
+
+
+	/**
+	 * Returns the PathMap of distances and whether the new path is shorter and thus
+	 * should be followed further.  This prevents endless cycles between nodes.
+	 */
+	def computePath(node: Node, oldDist: Option[Weight], newDist: Weight, distances: PathMap, list: List[Edge]): (PathMap, Boolean) = {
+		oldDist.map(x => {
+			if (x <= newDist) {
+				(distances, false)
+			} else {
+				(distances + ((node, list)), true)
+			}
+		}).getOrElse((distances + ((node, list)), true))
 	}
 
 }
