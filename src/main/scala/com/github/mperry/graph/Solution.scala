408b11f85292b10c20bd05f846bbc1241da1aba5
Mark Perry <maperry78@yahoo.com.au>
2014-03-31 09:39:57 +1000

    Read json from input file on command line.  Use gradle to set Scala command line

--- src/main/scala/com/github/mperry/graph/Solution.scala
+++ src/main/scala/com/github/mperry/graph/Solution.scala
@@ -1,10 +1,30 @@
 package com.github.mperry.graph
 
+import scala.io.Source
+
 object Solution {
 
-  def main(args: Array[String]) = {
-    println("Hi from scala2")
-  }
+	def main(args: Array[String]) = {
+		println("Hi from scala2")
+		val n = args.length
+		println(s"Scala command args: $args size: $n")
+		args.foreach(s => println(s"arg: $s"))
+		val filename = args(0)
+		println(s"file: $filename")
+		process(filename)
 
+	}
+
+	def process(filename: String) = {
+//		File(filename)
+		val source = Source.fromFile(filename)
+		val text = source.mkString
+		source.close()
+		processJson(text)
+	}
+
+	def processJson(json: String) = {
+		println(s"json: $json")
+	}
 
 }
