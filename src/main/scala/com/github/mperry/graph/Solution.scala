ffdbad8a3e8ff0ed76634591e33cc63a124c9160
Mark Perry <maperry78@yahoo.com.au>
2014-03-31 10:25:06 +1000

    Finished repl logic, need to process input

--- src/main/scala/com/github/mperry/graph/Solution.scala
+++ src/main/scala/com/github/mperry/graph/Solution.scala
@@ -2,22 +2,18 @@ package com.github.mperry.graph
 
 import scala.io.Source
 import com.github.mperry.graph.json.JsonHelper
+import scala.util.control.TailCalls.TailRec
+import scala.annotation.tailrec
 
 object Solution {
 
 	def main(args: Array[String]) = {
-//		println("Hi from scala2")
-		val n = args.length
-//		println(s"Scala command args: $args size: $n")
-//		args.foreach(s => println(s"arg: $s"))
 		val filename = args(0)
 		println(s"filename: $filename")
 		process(filename)
-
 	}
 
 	def process(filename: String) = {
-//		File(filename)
 		val source = Source.fromFile(filename)
 		val text = source.mkString
 		source.close()
@@ -27,40 +23,30 @@ object Solution {
 	def processJson(json: String) = {
 		println(s"json: $json")
 		val og = JsonHelper.parseJson(json)
-		val og2 = og.map(g => Graph.toGraph(g))
-		println(s"graph: $og2")
-		repl
-
+		val og2 = og.map(g => {
+			def g2 = Graph.toGraph(g)
+			println(s"graph: $g2")
+			repl(g2)
+		})
 	}
 
 	def nextLine(prompt: String): String = {
-
 		print(prompt)
 		readLine
-//		val lines = Source.fromInputStream(System.in).getLines()
-//		println(s"found lines $lines")
-//		""
 	}
 
-	def repl = {
-		println("Enter q to quit or json text")
-
-//		println("> ")
-
-		var loop = true
-		while (loop) {
-			val line = nextLine("> ")
-			val b = isQuit(line)
-			if (isQuit(line)) {
-				loop = false
-			} else {
-				processLine(line)
-			}
-
+	@tailrec
+	def step(g: Graph): Unit = {
+		val line = nextLine("> ")
+		if (!isQuit(line)) {
+			processLine(line)
+			step(g)
 		}
+	}
 
-//		for (ln <- io.Source.stdin.getLines) processLine(ln)
-
+	def repl(g: Graph) = {
+		println("Enter q to quit or json text")
+		step(g)
 	}
 
 	def isQuit(s: String): Boolean = {
